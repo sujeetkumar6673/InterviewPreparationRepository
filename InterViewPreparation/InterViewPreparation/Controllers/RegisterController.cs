6b4a92b8f30ba2b8e4c376f044e9e05fe5dcca0e
Sujeet <sujeet.kumar216@gmail.com>
2017-12-24 18:51:49 +0530

    Adding proect

---
+++ InterViewPreparation/InterViewPreparation/Controllers/RegisterController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InterViewPreparation.Models;
+
+namespace InterViewPreparation.Controllers
+{
+    public class RegisterController : Controller
+    {
+        InterviewPreparationEntities1 db = new InterviewPreparationEntities1();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(Person model)
+        {
+            db.Persons.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successful", JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult CheckVaildUser(Person model)
+        {
+            string result = "Fail";
+            var DataItem = db.Persons.Where(x => x.EmailId == model.EmailId && x.Password == model.Password).SingleOrDefault();
+            if (DataItem != null)
+            {
+                Session["UserID"] = DataItem.ID.ToString();
+                Session["UserName"] = DataItem.UserName.ToString();
+                result = "Success";
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult AfterLogin()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ InterViewPreparation/InterViewPreparation/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InterViewPreparation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InterViewPreparation")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e8283bc-932e-4d0e-9ba8-a286cfe74813")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
